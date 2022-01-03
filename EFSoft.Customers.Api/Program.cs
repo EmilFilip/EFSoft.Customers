53efa3286b9ae91ffbcb85dbd185a5665c3202ac
Emil Filip <emilfilip@hotmail.com>
2022-01-03 15:32:27 +0000

    Fixed connection string

--- EFSoft.Customers.Api/Program.cs
+++ EFSoft.Customers.Api/Program.cs
@@ -21,8 +21,8 @@ builder.Services.RegisterLocalServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c =>
@@ -30,7 +30,7 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Microservice V1");
     });
 
-}
+//}
 
 app.UseHttpsRedirection();
 
