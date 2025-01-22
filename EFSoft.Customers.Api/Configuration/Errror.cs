c69809cbf51f514d80dbb69436998997240cae39
EmilFilip <emilfilip@hotmail.com>
2025-01-22 11:26:07 +0000

    Added code for the implementetion of Result pattern

---
+++ EFSoft.Customers.Api/Configuration/Errror.cs
@@ -0,0 +1,15 @@
+﻿namespace EFSoft.Customers.Api.Configuration;
+
+public class Error
+{
+    public Error(
+        string message,
+        string code)
+    {
+        Message = message;
+        Code = code;
+    }
+
+    public string Message { get; }
+    public string Code { get; }
+}
